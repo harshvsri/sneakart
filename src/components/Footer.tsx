4b6759f39f60d5cd14739785c969749ee7a58baa
Harsh V Srivastava <harshvsri@gmail.com>
2023-07-01 19:18:20 +0530

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,13 @@ const Footer = () => {
     <>
       <footer className="text-center text-bg-dark p-3">
         <p className="m-0">&copy; 2023 ShoeKart, Inc</p>
+        <p className="m-0">
+          Created by{" "}
+          <a className="navLink" href="https://github.com/harshvsri">
+            Harsh
+          </a>{" "}
+          with ❣️
+        </p>
       </footer>
     </>
   );
